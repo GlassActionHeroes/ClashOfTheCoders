14e44ac281f3a87bf63b11a30572dff84e2ee57a
Galvin Butler <gbutler@bignerdranch.com>
2014-04-24 14:18:43 -0400

    Enabled switching between single player and collaborative modes and preserving each fragment instance.

--- Android/Nerd 2048/src/com/bignerdranch/android/nerd2048/GameActivity.java
+++ Android/Nerd 2048/src/com/bignerdranch/android/nerd2048/GameActivity.java
@@ -1,18 +1,21 @@
 package com.bignerdranch.android.nerd2048;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.webkit.WebView;
-import android.widget.Toast;
 
 public class GameActivity extends Activity {
 
-	private static final String EXTRA_GAME_MODE = "GameActivity.GameMode";
+	private static final String EXTRA_GAME_MODE = "GameActivity.ExtraGameMode";
+	private static final String SAVED_GAME_MODE = "GameActivity.SavedGameMode";
+	private static final String TAG_SINGLE_PLAYER_FRAGMENT = "GameActivity.TagSinglePlayerFragment";
+	private static final String TAG_COLLABORATIVE_FRAGMENT = "GameActivity.CollaborativeFragment";
 
 	public static final Intent newIntent(Context context, GameMode gameMode) {
 		Intent intent = new Intent(context, GameActivity.class);
@@ -21,16 +24,20 @@ public class GameActivity extends Activity {
 		return intent;
 	}
 
+	private GameMode mGameMode;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
 
-		Toast.makeText(this, getIntent().getSerializableExtra(EXTRA_GAME_MODE).toString(), Toast.LENGTH_SHORT).show();
+		mGameMode = (GameMode) getIntent().getExtras().get(EXTRA_GAME_MODE);
 
-		WebView webView = (WebView) findViewById(R.id.webview);
-		webView.getSettings().setJavaScriptEnabled(true);
-		webView.loadUrl("file:///android_asset/doge2048/index.html");
+		if (savedInstanceState != null) {
+			mGameMode = (GameMode) savedInstanceState.get(SAVED_GAME_MODE);
+		}
+
+		updateUI();
 	}
 
 	@Override
@@ -46,11 +53,52 @@ public class GameActivity extends Activity {
 			default:
 				return super.onOptionsItemSelected(item);
 			case R.id.single_player:
-				Toast.makeText(this, GameMode.SINGLE_PLAYER.toString(), Toast.LENGTH_SHORT).show();
+				if (mGameMode != GameMode.SINGLE_PLAYER) {
+					mGameMode = GameMode.SINGLE_PLAYER;
+					updateUI();
+				}
 				return true;
 			case R.id.collaborative:
-				Toast.makeText(this, GameMode.COLLABORATIVE.toString(), Toast.LENGTH_SHORT).show();
+				if (mGameMode != GameMode.COLLABORATIVE) {
+					mGameMode = GameMode.COLLABORATIVE;
+					updateUI();
+				}
 				return true;
 		}
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putSerializable(SAVED_GAME_MODE, mGameMode);
+	}
+
+	private void updateUI() {
+		FragmentTransaction ft = getFragmentManager().beginTransaction();
+
+		Fragment singlePlayerFragment = getFragmentManager().findFragmentByTag(TAG_SINGLE_PLAYER_FRAGMENT);
+		Fragment collaborativeFragment = getFragmentManager().findFragmentByTag(TAG_COLLABORATIVE_FRAGMENT);
+
+		if (mGameMode == GameMode.SINGLE_PLAYER) {
+			if (singlePlayerFragment == null) {
+				singlePlayerFragment = new SinglePlayerFragment();
+				ft.add(R.id.container, singlePlayerFragment, TAG_SINGLE_PLAYER_FRAGMENT);
+			}
+			if (collaborativeFragment != null) {
+				ft.hide(collaborativeFragment);
+			}
+			ft.show(singlePlayerFragment);
+		} else if (mGameMode == GameMode.COLLABORATIVE) {
+			if (collaborativeFragment == null) {
+				collaborativeFragment = new CollaborativeFragment();
+				ft.add(R.id.container, collaborativeFragment, TAG_COLLABORATIVE_FRAGMENT);
+			}
+			if (singlePlayerFragment != null) {
+				ft.hide(singlePlayerFragment);
+			}
+			ft.show(collaborativeFragment);
+		}
+
+		ft.commit();
 	}
 }
